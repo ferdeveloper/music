190ad427f9599fdb5b7c4fddbdf26fe86a431735
Fernando Arenas Alapont <fer@MacBook-Pro-de-Fernando.local>
2023-08-17 21:53:37 +0200

    Mobile song list added.

---
+++ src/components/molecules/SongList/SongList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="SongList">
+    <iframe
+      v-for="song in songListData"
+      :key="song.title"
+      :title="song.title"
+      :src="song.url"
+      width="100%"
+      height="120"
+      frameBorder="0"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"></iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import { Song } from '../../../types/Song';
+
+export default defineComponent({
+  name: 'SongList',
+  props: {
+    songListData: {
+      type: Array as PropType<Song[]>,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+  @import './SongList'
+</style>
---
+++ src/data/fallingData.ts
@@ -0,0 +1,38 @@
+import { Song } from '@/types/Song';
+
+const fallingData: Array<Song> = [
+  {
+    title: 'Sunrise',
+    url: 'https://open.spotify.com/embed/track/12QRiwZKmmVt8BVXkf29Gx',
+  },
+  {
+    title: 'The bronze door',
+    url: 'https://open.spotify.com/embed/track/5LlJyzzAvrzGNu2IorEx6P',
+  },
+  {
+    title: 'Whatever you want',
+    url: 'https://open.spotify.com/embed/track/0eHVcFB5b9VdUBbfDO6d31',
+  },
+  {
+    title: 'Run and don´t look back',
+    url: 'https://open.spotify.com/embed/track/2fdLFKKGi1pTTiO01Ic8dr',
+  },
+  {
+    title: 'SOL',
+    url: 'https://open.spotify.com/embed/track/2r95uOfBKzk5XLp54MNOKL',
+  },
+  {
+    title: 'The awakening of magic',
+    url: 'https://open.spotify.com/embed/track/1737OdEpB4SIiucZfwCeiL',
+  },
+  {
+    title: 'Faster than time',
+    url: 'https://open.spotify.com/embed/track/0usqM2pIde1orXGYWoLqXw',
+  },
+  {
+    title: 'Seven in the morning',
+    url: 'https://open.spotify.com/embed/track/1iHryiciPbJHwNl5HEXhms',
+  },
+];
+
+export default fallingData;
