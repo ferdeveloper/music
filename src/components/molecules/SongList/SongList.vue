f116d34c88ab9a061f19f793696e3f9904669016
Fernando Arenas Alapont <fer@MacBook-Pro-de-Fernando.local>
2023-08-17 22:03:07 +0200

    Desktop view done.

--- src/components/molecules/SongList/SongList.vue
+++ src/components/molecules/SongList/SongList.vue
@@ -6,7 +6,7 @@
       :title="song.title"
       :src="song.url"
       width="100%"
-      height="120"
+      height="152"
       frameBorder="0"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"></iframe>
