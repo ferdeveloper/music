8d8198a0eef433b454550f086a774db5317051a2
Fernando Arenas Alapont <fer@MacBook-Pro-de-Fernando.local>
2023-08-21 23:17:26 +0200

    New component added.

--- src/views/FallingView.vue
+++ src/views/FallingView.vue
@@ -5,8 +5,12 @@
       title="Falling"
       alt="Falling"
     />
-    <SongList
-      :songListData="fallingData"
+    <AlbumInfo
+      info="Falling es un album instrumental ideal para escuchar de manera relajada.
+      Sus temas son composiciones cíclicas que van in crescendo desde melodías intimas de piano
+      hasta grandes fragmentos orquestales. Una definición que me gusta es que falling es ideal
+      para ser escuchado programando, pero sinceramente, sus composiciones van mucho mas allá."
+      url="https://open.spotify.com/embed/album/5vvHdYI1C21dnS8Mon45B8?utm_source=generator&theme=0"
     />
   </div>
 </template>
@@ -14,14 +18,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AlbumHero from '@/components/molecules/AlbumHero/AlbumHero.vue';
-import SongList from '@/components/molecules/SongList/SongList.vue';
+import AlbumInfo from '@/components/molecules/AlbumInfo/AlbumInfo.vue';
 import fallingData from '../data/fallingData';
 
 export default defineComponent({
   name: 'FallingView',
   components: {
     AlbumHero,
-    SongList,
+    AlbumInfo,
   },
   setup() {
     return {
